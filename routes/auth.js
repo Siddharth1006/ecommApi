44c1b204d5e6169619d96d5904939e9f34082998
Siddharth S <siddharthsoma01@gmail.com>
2022-08-04 03:39:15 +0530

    Auth Login Endpoint created
    In this login endpoint, we decrypt the encrypted password matching the username and compare it with the password given by the user as input. If both are same, user is returned to the else Error message is displayed

--- routes/auth.js
+++ routes/auth.js
@@ -17,13 +17,41 @@ router.post('/register', async (req, res) => {
         ).toString(),
     });
 
-    //To send it to our db we can use save()
     try {
-        //Instead of console logging we are using status check at client side.
-        //200 - Successful!
-        //201 - Successfully Added
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN
+router.post('/login', async (req, res) => {
+    //To send it to our db we can use save()
+    try {
+        //findOne because there is only 1 user with same username
+        const searchUser = await User.findOne({ username: req.body.username });
+        //if you find the "searchUser" then return their db ONLY WHEN PASSWORD IS RIGHT!
+        //The password will be hashed. So we need to DECRYPT IT!
+
+        !searchUser && res.status(401).json('Wrong Credentials!');
+        //if there is no SearchUser then return 401 Error with above message
+
+        const hashedPasswordfound = CryptoJS.AES.decrypt(
+            searchUser.password,
+            process.env.PASSWORD_SEC_KEY,
+        );
+
+        const OriginalPassword = hashedPasswordfound.toString(
+            CryptoJS.enc.Utf8,
+        );
+        const InputPassword = req.body.password;
+        // If the password that was found IS NOT SAME AS THE password mapped to the given username
+        // then just return 401 error
+        OriginalPassword !== InputPassword && res.status(201).json(searchUser);
+
+        // If its correct, then return the user!
+        res.status(200).json(searchUser);
     } catch (err) {
         res.status(500).json(err);
         //500 status code for error
